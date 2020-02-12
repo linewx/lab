039899ee4e6310accec41674acc587064348ef61
ganlin.lu <ganlin.lu@zkjin.com>
2020-02-12 17:24:25 +0800

    add gc test

---
+++ java/jvm/src/main/java/com/zz/lab/jvm/GCTest.java
@@ -0,0 +1,35 @@
+package com.zz.lab.jvm;
+
+
+/**
+ * VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * simulate heap out of memory
+ * output:
+ * Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+ */
+public class GCTest {
+    public static class OOMObject {
+        OOMObject() {
+
+        }
+    }
+
+    public static void main(String...argv) {
+        byte[] a1, a2, a3, a4, a5, a6;
+        int mb = 1000 * 1000;
+        a1 = new byte[2 * mb];
+        a2 = new byte[2 * mb];
+        a3 = new byte[2 * mb];
+        a4 = new byte[4 * mb];
+        //a5 = new byte[4 * mb];
+        //a6 = new byte[4 * mb];
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.gc();
+        //System.out.println(a5);
+        //System.out.println(a6);
+    }
+}
