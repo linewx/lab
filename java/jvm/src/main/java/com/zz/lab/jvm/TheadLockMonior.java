ae785fc9e11f710fd10d806ca31b020317cfe371
ganlin.lu <ganlin.lu@zkjin.com>
2020-02-12 18:46:12 +0800

    add more test for it

---
+++ java/jvm/src/main/java/com/zz/lab/jvm/TheadLockMonior.java
@@ -0,0 +1,49 @@
+package com.zz.lab.jvm;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * run with JProfiler, see thread tab: thread monitor, thread dump, thread history
+ */
+public class TheadLockMonior {
+    /**
+     * 线程死锁等待演示
+     */
+    static class SynAddRunalbe implements Runnable {
+        int a, b;
+        public SynAddRunalbe(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public void run() {
+            synchronized (Integer.valueOf(a)) {
+                synchronized (Integer.valueOf(b)) {
+                    try {
+
+                    Thread.sleep(1000);
+                    } catch (Exception e) {
+
+                    }
+                    System.out.println(a + b);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        for (int i = 0; i < 100; i++) {
+            new Thread(new SynAddRunalbe(1, 2)).start();
+            new Thread(new SynAddRunalbe(2, 1)).start();
+        }
+
+        Thread.sleep(100000);
+    }
+
+
+
+}
