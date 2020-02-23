80060ff541ff8a1276c31cbc3691a0639b74669c
ganlin.lu <ganlin.lu@zkjin.com>
2020-02-23 13:21:37 +0800

    refactor code

--- java/neo4j/src/test/java/com/zz/lab/TestGraph.java
+++ java/neo4j/src/test/java/com/zz/lab/TestGraph.java
@@ -1,17 +1,57 @@
 package com.zz.lab;
 
+import com.zz.lab.entity.Person;
+import com.zz.lab.repo.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class TestGraph {
+    @Autowired
+    private PersonRepository personRepository;
 
     @Test
     public void testGraph() {
+        personRepository.deleteAll();
 
+        Person greg = new Person("Greg");
+        Person roy = new Person("Roy");
+        Person craig = new Person("Craig");
+
+        List<Person> team = Arrays.asList(greg, roy, craig);
+
+        log.info("Before linking up with Neo4j...");
+
+        team.stream().forEach(person -> log.info("\t" + person.toString()));
+
+        personRepository.save(greg);
+        personRepository.save(roy);
+        personRepository.save(craig);
+
+        greg = personRepository.findByName(greg.getName());
+        greg.worksWith(roy);
+        greg.worksWith(craig);
+        personRepository.save(greg);
+
+        roy = personRepository.findByName(roy.getName());
+        roy.worksWith(craig);
+        // We already know that roy works with greg
+        personRepository.save(roy);
+
+        // We already know craig works with roy and greg
+
+        log.info("Lookup each person by name...");
+        team.stream().forEach(person -> log.info(
+                "\t" + personRepository.findByName(person.getName()).toString()));
 
     }
 
